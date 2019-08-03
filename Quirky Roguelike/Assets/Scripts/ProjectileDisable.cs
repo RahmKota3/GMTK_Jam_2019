d8efda877f8396c02ec1cdabd50cde7211de56d3
RahmKota3 <38017498+RahmKota3@users.noreply.github.com>
2019-08-03 16:43:09 +0200

    Minor fixes (projectile, hole)

---
+++ Quirky Roguelike/Assets/Scripts/ProjectileDisable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDisable : MonoBehaviour
+{
+    [SerializeField]
+    float maxLifeTime = 3;
+
+    float lifeTimer = 0;
+
+    private void OnEnable()
+    {
+        lifeTimer = 0;
+    }
+
+    private void Update()
+    {
+        lifeTimer += Time.deltaTime;
+
+        if (lifeTimer >= maxLifeTime)
+            SimplePool.Despawn(gameObject);
+    }
+}
