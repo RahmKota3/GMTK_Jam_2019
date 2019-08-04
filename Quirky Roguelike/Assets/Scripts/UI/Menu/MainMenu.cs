8f44d434dec0a223a07de52120e3a86eac64ead1
Paweł Rajkowski <48755361+Rajkowski01@users.noreply.github.com>
2019-08-04 16:19:21 +0200

    haha

--- Quirky Roguelike/Assets/Scripts/UI/Menu/MainMenu.cs
+++ Quirky Roguelike/Assets/Scripts/UI/Menu/MainMenu.cs
@@ -8,12 +8,8 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Menu");
     }
 
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
 
 }
