852b76a7197df8261f978ba56286c00bcf784102
infernosynek <michal.rajkowski@wp.pl>
2019-08-04 16:01:59 +0200

    Sceny(działa)

--- Quirky Roguelike/Assets/Scripts/Niewiemgdzietodać/SceneManagerScript.cs
+++ Quirky Roguelike/Assets/Scripts/Niewiemgdzietodać/SceneManagerScript.cs
@@ -23,12 +23,13 @@ public class SceneManagerScript : MonoBehaviour
     //============KONIEC SINGLETONA=======================
     
     public enum Scene {
-        BombLevel, HalfScreenVisible
+        BombLevel, HalfScreenVisible, CrazyProjectile, MoveByShooting
     }
     public Scene currentScene;
     //nazwa enum scen ma być taka sama jak sceny do losowanie, trzeba dodać wszystkie enumy tu V
-    public static List<Scene> TestoweSceny = new List<Scene>() { Scene.BombLevel, Scene.HalfScreenVisible };
+    public static List<Scene> TestoweSceny = new List<Scene>() { Scene.BombLevel, Scene.HalfScreenVisible, Scene.CrazyProjectile, Scene.MoveByShooting };
     public int numberOfScenes = 3;
+    int previousScene = 999;
     public Scene RandomScene()
     {
         Scene nextScene;
@@ -36,8 +37,16 @@ public class SceneManagerScript : MonoBehaviour
         // Scene[] sceny = { Scene.Test1, Scene.Test2, Scene.Test3 };
         int randomIndex = 0;
 
-            
-        randomIndex = Random.Range(0, numberOfScenes); //zmienic size na coś normalnego xd
+            while (true)
+        {
+            randomIndex = Random.Range(0, numberOfScenes);
+            if (randomIndex != previousScene)
+            {
+                previousScene = randomIndex;
+                break;
+            }
+        }
+        //zmienic size na coś normalnego xd
                 
         
         nextScene = TestoweSceny[randomIndex];
